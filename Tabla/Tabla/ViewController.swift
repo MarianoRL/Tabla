6755d479e42f9a28b224fdc8676c0962c614a1c9
OscarBM <oscarismaelbm@gmail.com>
2019-03-28 19:37:55 -0600

    first commit

---
+++ Tabla/Tabla/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Tabla
+//
+//  Created by Alumno IDS on 3/28/19.
+//  Copyright © 2019 Alumno IDS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
